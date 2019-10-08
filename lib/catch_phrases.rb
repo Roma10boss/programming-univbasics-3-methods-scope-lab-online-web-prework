0862f89f95f18c8008d6513e33c446cdcb258fd1
Romario Gustave <romagustave10@gmail.com>
2019-10-08 05:53:55 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,8 +4,9 @@ def mario()
   puts phrase
 end
 mario()
-def toadstool(status)
+def toadstool()
    status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+toadstool
 
