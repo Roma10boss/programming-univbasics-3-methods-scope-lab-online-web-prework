e3050a0ed963165679230741de16976c0c21e824
Romario Gustave <romagustave10@gmail.com>
2019-10-08 05:56:07 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,12 +1,12 @@
 
-def mario
+def mario(phrase)
   phrase= "It's-a me Mario!"
   puts phrase
 end
-mario
-def toadstool
+mario(phrase)
+def toadstool(status)
    status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
-toadstool
+toadstool(status)
 
